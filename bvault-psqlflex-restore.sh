79b4bfd3da6c5b63ca38583885a6489aba79a551
Dominik Kopec <kopec.dominik@gmai.com>
2024-08-29 16:23:40 -0400

    feature: Backup Vault PSQL Flex Instance Restore script
    The script uses the Azure REST api to trigger a restore to a storage account container.
    
    You can pass in the RECOVERY_POINT_TIME as "latest" and it will pull the latest by date, otherwise it needs the recoveryPointTime property in proper timestamp format.
    
    The BACKUP_INSTANCE_NAME can be the friendly name (what the portal shows) it will reach out and get the proper id based on that.
    
    RESOURCE_GROUP, SUBSCRIPTION_ID, refer to the VAULT_NAME, properties.
    
    RESTORE_LOCATION needs to be the azure region, not sure if its of the storage or the vault.
    
    Note as of this commit az rest does not have a header return so using --debug 2>debug_request.txt is the only way to get the Azure-AsyncOperation URL.

---
+++ bvault-psqlflex-restore.sh
@@ -0,0 +1,79 @@
+#region USER VARIABLES
+export RECOVERY_POINT_TIME="your_recovery_point_time"
+export SUBSCRIPTION_ID="your_subscription_id"
+export RESOURCE_GROUP="your_resource_group"
+export VAULT_NAME="your_vault_name"
+export BACKUP_INSTANCE_NAME="your_backup_instance_name"
+export STORAGE_URL="your_storage_url"
+export FILE_PREFIX="your_file_prefix"
+export RESTORE_LOCATION="your_restore_location"
+#endregion
+
+export LIST_BACKUPVAULTS="https://management.azure.com/subscriptions/${SUBSCRIPTION_ID}/resourceGroups/${RESOURCE_GROUP}/providers/Microsoft.DataProtection/backupVaults?api-version=2021-07-01"
+export LIST_BACKUPINSTANCES="https://management.azure.com/subscriptions/${SUBSCRIPTION_ID}/resourceGroups/${RESOURCE_GROUP}/providers/Microsoft.DataProtection/backupVaults/${VAULT_NAME}/backupInstances/?api-version=2021-07-01"
+export BACKUP_INSTANCE_ACTUAL_NAME=$(az rest --method get --url $LIST_BACKUPINSTANCES --query "value[?properties.friendlyName=='${BACKUP_INSTANCE_NAME}'].name" -o tsv)
+export LIST_RECOVERYPOINTS="https://management.azure.com/subscriptions/${SUBSCRIPTION_ID}/resourceGroups/${RESOURCE_GROUP}/providers/Microsoft.DataProtection/backupVaults/${VAULT_NAME}/backupInstances/${BACKUP_INSTANCE_ACTUAL_NAME}/recoveryPoints?api-version=2021-07-01"
+export RECOVERY_POINT_ID=$(az rest --method get --url $LIST_RECOVERYPOINTS --query "max_by(value[*],&properties.recoveryPointTime).properties.recoveryPointId" -o tsv)
+
+if [ "$RECOVERY_POINT_TIME" != "latest" ]; then
+  export RECOVERY_POINT_ID=$(az rest --method get --url $LIST_RECOVERYPOINTS --query "value[?properties.recoveryPointTime=='${RECOVERY_POINT_TIME}'].properties.recoveryPointId" -o tsv)
+  echo "Set the Recovery point to $RECOVERY_POINT_ID taken at $RECOVERY_POINT_TIME"
+fi
+
+export VALIDATE_RESTORE="https://management.azure.com/subscriptions/${SUBSCRIPTION_ID}/resourceGroups/${RESOURCE_GROUP}/providers/Microsoft.DataProtection/backupVaults/${VAULT_NAME}/backupInstances/${BACKUP_INSTANCE_ACTUAL_NAME}/validateRestore?api-version=2021-07-01"
+
+printf '{
+  "objectType": "ValidateRestoreRequestObject",
+  "restoreRequestObject": {
+    "objectType": "AzureBackupRecoveryPointBasedRestoreRequest",
+    "sourceDataStoreType": "VaultStore",
+    "restoreTargetInfo": {
+      "targetDetails": {
+        "url": "%s",
+        "filePrefix": "%s",
+        "restoreTargetLocationType": "AzureBlobs"
+      },
+      "restoreLocation": "%s",
+      "recoveryOption": "FailIfExists",
+      "objectType": "RestoreFilesTargetInfo"
+    },
+    "recoveryPointId": "%s"
+  }
+}' "$STORAGE_URL" "$FILE_PREFIX" "$RESTORE_LOCATION" "$RECOVERY_POINT_ID" >test.json
+
+az rest --method post --url $VALIDATE_RESTORE --body @test.json --debug 2>debug_verification.txt
+
+export TRIGGER_RESTORE="https://management.azure.com/subscriptions/${SUBSCRIPTION_ID}/resourceGroups/${RESOURCE_GROUP}/providers/Microsoft.DataProtection/backupVaults/${VAULT_NAME}/backupInstances/${BACKUP_INSTANCE_ACTUAL_NAME}/restore?api-version=2021-07-01"
+
+printf '{
+  "objectType": "AzureBackupRecoveryPointBasedRestoreRequest",
+  "sourceDataStoreType": "VaultStore",
+  "restoreTargetInfo": {
+    "targetDetails": {
+        "url": "%s",
+        "filePrefix": "%s",
+        "restoreTargetLocationType": "AzureBlobs"
+    },
+    "restoreLocation": "%s",
+    "recoveryOption": "FailIfExists",
+    "objectType": "RestoreFilesTargetInfo"
+  },
+  "recoveryPointId": "%s"
+}' "$STORAGE_URL" "$FILE_PREFIX" "$RESTORE_LOCATION" "$RECOVERY_POINT_ID" > request.json
+
+az rest --method post --url "$TRIGGER_RESTORE" --body @request.json --debug 2>debug_request.txt
+
+export RESULT_ASYNC_URL=$(grep -oP "'Azure-AsyncOperation': '\K\S+" "./debug_request.txt" | sed "s/'$//")
+
+succeeded=false
+
+while [ "$succeeded" != "Succeeded" ]; do
+  succeeded=$(az rest --method get --url $RESULT_ASYNC_URL | grep -oP '"status": "\K[^"]+')
+  echo "Restore Job status is: $succeeded"
+  # Check the value of succeeded
+  if [ "$succeeded" == "Failed" ] || [ "$succeeded" == "Canceled" ]; then
+    echo "Error: Restore Operation $succeeded"
+    exit 1
+  fi
+  sleep 10 # Wait for a 10 seconds before checking again
+done
